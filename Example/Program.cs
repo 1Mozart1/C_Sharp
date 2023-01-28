92f29fa9d0faf15dedcd241abcc2bc9eb72f34af
1Mozart1 <andreivm2015@yandex.ru>
2023-01-28 19:17:32 +0500

    Seminar

--- Example/Program.cs
+++ Example/Program.cs
@@ -25,3 +25,14 @@
 // int res = numA * numB;
 // Console.WriteLine(res);
 
+Console.Write ("Введите имя пользователя: ");
+string? username = Console.ReadLine();
+if(username.ToLower() == "маша") // ToLower - переводит все символы строки в нижний регистр
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
