90f267201bf7c4cdbdd98c1a8e8c2d6c79e19ec4
1Mozart1 <andreivm2015@yandex.ru>
2023-01-28 18:45:53 +0500

    seminar

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,4 +1,27 @@
-﻿Console.Write("Введите ваше имя: ");
-string username = Console.ReadLine ();
-Console.Write("Привет, ");
-Console.WriteLine(username);
+﻿// Console.Write("Введите ваше имя: "); 
+// string username = Console.ReadLine (); // введение данных пользователем
+// Console.Write("Привет, ");
+// Console.WriteLine(username);
+
+// int numA = 31;
+// int numВ = 5;
+// int res = numA + numВ;
+// Console.WriteLine(res);
+
+// double numA = 31; // тип данных, который используется для переменных, содержащих числа с дробной частью
+// double numB = 15;
+// double res = (numA / numB);
+// Console.WriteLine(res);
+
+// int numA = new Random().Next(-100,100); // рамдомное число
+// int numB = new Random().Next(-100,100);
+// int res = numA * numB;
+// Console.WriteLine(res);
+
+// int numA = new Random().Next(-100,100);
+// Console.WriteLine(numA); // что бы видеть рандомные числа
+// int numB = new Random().Next(-100,100);
+// Console.WriteLine(numB);
+// int res = numA * numB;
+// Console.WriteLine(res);
+
