d5576900e6059a4ecf433aeddc05ce0175b68c09
1Mozart1 <andreivm2015@yandex.ru>
2023-01-28 20:53:56 +0500

    Seminar

--- Example/Program.cs
+++ Example/Program.cs
@@ -64,18 +64,21 @@ while (count < 10);
         x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
+
     if (what == 1);
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
-        if (what == 2);
+    
+    if (what == 2);
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
     }
+
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
-    count++;    //
+    count++;    // тоже самое count = count + 1, или count += 1
 }
 
