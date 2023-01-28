cc84a711cd2b3af62809fb17de99903b23561e40
1Mozart1 <andreivm2015@yandex.ru>
2023-01-28 20:08:56 +0500

    Seminar

--- Example/Program.cs
+++ Example/Program.cs
@@ -25,14 +25,62 @@
 // int res = numA * numB;
 // Console.WriteLine(res);
 
-Console.Write ("Введите имя пользователя: ");
-string? username = Console.ReadLine();
-if(username.ToLower() == "маша") // ToLower - переводит все символы строки в нижний регистр
-{
-    Console.WriteLine("Ура, это же МАША");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+// Console.Write ("Введите имя пользователя: ");
+// string? username = Console.ReadLine();
+// if(username.ToLower() == "маша") // ToLower - переводит все символы строки в нижний регистр
+// {
+//     Console.WriteLine("Ура, это же МАША");
+// }
+// else
+// {
+//     Console.Write("Привет, ");
+//     Console.WriteLine(username);
+// }
+
+Console.Clear();
+int xa = 1, ya = 1, 
+    xb = 1, yb = 15, 
+    xc = 15, yc = 10;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+//Console.SetCursorPosition(xb, yb);
+//Console.WriteLine("+");
+
+//Console.SetCursorPosition(xc, yc);
+//Console.WriteLine("+");
+// {
+// int what = new Random().Next(0, 3);
+// }
+// int x = xa, y = xb
+// int count = 0;
+// while(count < 10)
+// {
+// int what = new Random().Next(0, 3);
+// if (what == 0)
+// {
+// x = (x + xa) / 2;
+// y = (y + ya) / 2;
+// }
+// {
+// int what = new Random().Next(0, 3);
+// if (what == 0)
+// {
+// x = (x + xa) / 2;
+// y = (y + ya) / 2;
+// }
+// if (what == 1)
+// {
+// x = (x + xb) / 2;
+// y = (y + yb) / 2;
+// }
+// if (what == 2)
+// {
+// x = (x + xс) / 2;
+// y = (y + yс) / 2;
+// }
+// Console.Set.CursorPosition(x, y);
+// Console.WriteLine(“+”)
+// count++
+// }
