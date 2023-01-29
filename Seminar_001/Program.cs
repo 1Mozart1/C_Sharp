8cf461e4e7d4e696f73f38fe030c8d74729ceac1
1Mozart1 <andreivm2015@yandex.ru>
2023-01-29 12:37:57 +0500

    example_01

--- Seminar_001/Program.cs
+++ Seminar_001/Program.cs
@@ -1,46 +1,5 @@
-﻿Console.Clear();
-//Console.SetCursorPosition(10, 4);
-//Console.WriteLine("+");
-
-int xa = 40, ya = 1, 
-    xb = 1, yb = 20, 
-    xc = 80, yc = 20;
-
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
-
-Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
-
-Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
-
-int x = xa, y = xb;
-
-int count = 0;
-
-while (count < 10);
-{
-    int what = new Random().Next(0, 3);
-    if (what == 0);
-    {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
-    }
-
-    if (what == 1);
-    {
-        x = (x + xb) / 2;
-        y = (y + yb) / 2;
-    }
-    
-    if (what == 2);
-    {
-        x = (x + xc) / 2;
-        y = (y + yc) / 2;
-    }
-
-    Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
-    count++;    // тоже самое count = count + 1, или count += 1
-}
+﻿System.Console.Write("Введите число: "); // 
+int num = Convert.ToInt32(Console.ReadLine());// (int.Parse() || Convert.ToInt32() - конвертирование строки) - ввод данных пользователем
+int res = num * num;
+System.Console.WriteLine("Квадрат числа равен: " + res + " Text");
+System.Console.WriteLine($"Квадрат числа равен: {res} Text");
