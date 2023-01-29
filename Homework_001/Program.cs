3caed13436fefac1abee9f279b6db84429d19e49
1Mozart1 <andreivm2015@yandex.ru>
2023-01-29 14:12:24 +0500

    Homework Task № 3

--- Homework_001/Program.cs
+++ Homework_001/Program.cs
@@ -17,17 +17,30 @@
 
 ////////////////////////////////// Задача "максимальное из 3-х чисел." ////////////////////////////////////
 
-System.Console.Write("Введите число а: ");
-int num_A = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите число b: ");
-int num_B = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите число c: ");
-int num_C = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число а: ");
+// int num_A = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число b: ");
+// int num_B = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число c: ");
+// int num_C = Convert.ToInt32(Console.ReadLine());
 
-int max = num_A;
+// int max = num_A;
 
-if (num_B > max) max = num_B;
-if (num_C > max) max = num_C;
+// if (num_B > max) max = num_B;
+// if (num_C > max) max = num_C;
 
-System.Console.WriteLine("Максимальное число " + max);
+// System.Console.WriteLine("Максимальное число " + max);
 
+/////////////////////////////////// Задача "является ли число четным" //////////////////////////////////////
+
+System.Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    System.Console.WriteLine("Число четное");
+}
+else
+{
+    System.Console.WriteLine("Число нечетное");
+}
