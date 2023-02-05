71a8867365f10b946ce75d1658aea5d7085ac079
1Mozart1 <andreivm2015@yandex.ru>
2023-02-05 21:24:28 +0500

    Homework_003_task # 2

--- Homework_003/Program.cs
+++ Homework_003/Program.cs
@@ -1,22 +1,40 @@
-﻿
+﻿///// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
 
 
 
 
+///// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
+// int input_num(string str)
+// {
+//     System.Console.Write(str);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
 
+// int x1 = input_num("Введите координату x1: ");
+// int y1 = input_num("Введите координату y1: ");
+// int z1 = input_num("Введите координату z2: ");
+// int x2 = input_num("Введите координату x2: ");
+// int y2 = input_num("Введите координату y2: ");
+// int z2 = input_num("Введите координату z2: ");
 
+// double distance = (Math.Round(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2)), 3));
+// System.Console.WriteLine($"Расстояние между точками " + distance);
 
-
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
 
 ///// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-System.Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
-for (int count = 1; count <= a; count++)
-{
-    System.Console.Write(Math.Pow(count, 3) + ", ");
-}
+// for (int count = 1; count <= a; count++)
+// {
+//     System.Console.Write(Math.Pow(count, 3) + ", ");
+// }
