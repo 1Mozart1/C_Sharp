0fdcd3e6e2cdb11bbcf709638d41fee3ff3533e9
1Mozart1 <andreivm2015@yandex.ru>
2023-02-07 19:03:09 +0500

    Seminar_003

--- Example/Program.cs
+++ Example/Program.cs
@@ -158,52 +158,220 @@
 
 // void - не возвращает значения
 
-void FillArray(int[] collection) // создаёт массив
+// void FillArray(int[] collection) // создаёт массив
 
-{
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
-    {
-        collection[index] = new Random().Next(1, 10);
-        index++;
-    }
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         index++;
+//     }
 
-}
+// }
 
-void PrintArray(int[] col) // Печатает массив
-{
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
-        System.Console.WriteLine(col[position]);
-        position++;
-    }
-}
+// void PrintArray(int[] col) // Печатает массив
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         System.Console.WriteLine(col[position]);
+//         position++;
+//     }
+// }
 
-int IndexOf(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = -1; // указываем -1 для того чтобы показывал несуществующий элемент
-    while (index < count)
-    {
-        if (collection[index] == find)
-        {
-            position = index;
-            break;
-        }
-        index++;
-    }
-    return position;
-}
+// int IndexOf(int[] collection, int find)
+// {
+//     int count = collection.Length;
+//     int index = 0;
+//     int position = -1; // указываем -1 для того чтобы показывал несуществующий элемент
+//     while (index < count)
+//     {
+//         if (collection[index] == find)
+//         {
+//             position = index;
+//             break;
+//         }
+//         index++;
+//     }
+//     return position;
+// }
 
-int[] array = new int[10]; // указываем количество элементов в массиве
+// int[] array = new int[10]; // указываем количество элементов в массиве
 
-FillArray(array); // заполняет массив
-PrintArray(array); // выводит на экран созданный массив
-System.Console.WriteLine();
+// FillArray(array); // заполняет массив
+// PrintArray(array); // выводит на экран созданный массив
+// System.Console.WriteLine();
 
-int pos = IndexOf(array, 14);
-System.Console.WriteLine(pos);
+// int pos = IndexOf(array, 14);
+// System.Console.WriteLine(pos);
+
+
+//********************** Lesson # 3 *************************************************************************************
+
+// Виды методов
+
+// 1 группа (ничего не принимаю и ничего не возвращают)**********************************
+
+//void Method1() // void- имя метода, потом название
+// {
+//     System.Console.WriteLine("Автор ...");
+// }
+//Method1(); // скобки обязательно
+
+
+// 2 группа (ничего не возвращают, но могут принимать данные)******************************
+
+//void Method2(string msg)
+// {
+//     System.Console.WriteLine(msg);
+// }
+//Method2(msg: "Текст");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         System.Console.WriteLine(msg);
+//         i++;
+//     }
+
+// }
+//Method21("Текст", 4);
+//Method21(msg: "Текст", count: 4);  // можно присваивать конкретному значению
+//Method21(count: 4, msg: "Текст" ); // необязательно по порядку
+
+
+// 3 группа (что-то возвращают, но ничего не принимают)*******************************
+
+// int Method3()  // Обязательно должны указать тип данных
+// {
+//     return DateTime.Now.Year; // return - обязательно оператор возврата
+// }
+// int Year = Method3();
+// System.Console.WriteLine(Year);
+
+
+// 4 группа (что-то принимаю и что-то возвращают)*************************************
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = string.Empty; // для начала должны быть хоть какае- нибудь значения, в данном случае "пустая строка" 
+
+//     while (i < count)
+//     {
+//         result = result + text;    
+//         i++;
+//     }
+//     return result;
+
+// }
+// string res = Method4(10, "z");
+// System.Console.WriteLine(res);
+
+// Через цикл FOR **************************************
+
+// string Method4(int count, string text)
+// {
+//     string result = string.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+
+// }
+// string res = Method4(10, "z");
+// System.Console.WriteLine(res);
+
+// Цикл в Цикле - таблица уножения
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         System.Console.WriteLine($"{i} * {j} = {i*j}");
+//     }
+//     System.Console.WriteLine(""); // чтобы разделить строки
+// }
+
+
+// Задача Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы “к” заменить большими “К”, *****************
+// а большие “В” заменить маленькими “в”.
+
+// string s = "qwerty"
+//             012345
+// s[3] - r
+
+// string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде, "
+//             + "вы бы взяли приступом согласие прусского короля."
+//             + "Вы так красноречивы. Выдадите мне чаю? ";
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+
+//     int Length = text.Length;
+//     for (int i = 0; i < Length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|'); // указавыем где мы хотим заменить, что хотим заменитью на что хотим заменить
+// System.Console.WriteLine(newText);
+
+// System.Console.WriteLine();
+
+// newText = Replace(newText, 'к', 'К');
+// System.Console.WriteLine(newText);
+
+// System.Console.WriteLine();
+
+// newText = Replace(newText, 'В', 'в');
+// System.Console.WriteLine(newText);
+
+
+// Сортировка массива *******************************************
+
+// int[] arr = { 1, 4, 6, 3, 2, 7, 5, 1, 1 };
+
+// void PrintArray(int[] array) // новый массив
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         System.Console.Write($"{array[i]} ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+
+//     }
+// }
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
