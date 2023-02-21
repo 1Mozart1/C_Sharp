6eab13f61349b38844a6ae31435872d2b96af880
1Mozart1 <andreivm2015@yandex.ru>
2023-02-21 23:58:50 +0500

    Seminar_008

--- Seminar_001/Program.cs
+++ Seminar_001/Program.cs
@@ -1244,3 +1244,340 @@
 // PrintMatrix(myMatrix);
 // System.Console.WriteLine($"Сумма элементов главной диагонали = {SumElements(myMatrix)}");
 
+
+
+// Lesson # 8  Двумерный массивы    ********************************************************************************************
+
+// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int[,] GenerateMatrix(int rows, int cols)
+// {
+//     Random rand = new Random();
+//     int[,] matrix = new int[rows, cols];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rand.Next(0, 11);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i, j] + "\t"); // \t - это табуляция (tab)
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void Exchange(int[,] matrix)
+// {
+//     int temp;
+//     for (int i = 0; i < matrix.GetLength(1); i++)
+//     {
+//         temp = matrix[0, i];
+//         matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+//         matrix[matrix.GetLength(0) - 1, i] = temp;
+//     }
+// }
+
+// int rows = ReadInt("Введите количество строк: ");
+// int cols = ReadInt("Введите количество столбцов: ");
+
+// var myMatrix = GenerateMatrix(rows, cols);
+// PrintMatrix(myMatrix);
+// System.Console.WriteLine();
+// Exchange(myMatrix);
+// PrintMatrix(myMatrix);
+
+
+//Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. -----------------------------
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int[,] GenerateMatrix(int rows, int cols)
+// {
+//     Random rand = new Random();
+//     int[,] matrix = new int[rows, cols];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rand.Next(0, 11);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i, j] + "\t"); // \t - это табуляция (tab)
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// int[,] TurnOver(int[,] matrix)
+// {
+//     int[,] matrix2 = new int[matrix.GetLength(1), matrix.GetLength(0)];
+//     for (int i = 0; i < matrix.GetLength(1); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(0); j++)
+//         {
+//             matrix2[i, j] = matrix[j, i];
+//         }
+//     }
+//     return matrix2;
+// }
+
+
+// int rows = ReadInt("Введите количество строк: ");
+// int cols = ReadInt("Введите количество столбцов: ");
+
+// var myMatrix = GenerateMatrix(rows, cols);
+// PrintMatrix(myMatrix);
+// System.Console.WriteLine();
+// int[,] myMatrix2 = TurnOver(myMatrix);
+// PrintMatrix(myMatrix2);
+
+
+// Задача 57: Составить частотный словарь элементов двумерного массива. -----------------------------------------------------
+//            Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.-------------------
+
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int[,] GenerateMatrix(int rows, int cols)
+// {
+//     Random rand = new Random();
+//     int[,] matrix = new int[rows, cols];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rand.Next(0, 11);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i, j] + "\t"); // \t - это табуляция (tab)
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void Count(int[,] matrix)
+// {
+//     int count = 0;
+//     for (int l = 0; l < 11; l++)
+//     {
+//         count = 0;
+//         for (int i = 0; i < matrix.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < matrix.GetLength(1); j++)
+//             {
+//                 if (l == matrix[i, j]) count++;
+
+//             }
+//         }
+//         if (count > 0)
+//             System.Console.WriteLine($"{l} встречается {count} раз(а)");
+//     }
+// }
+
+
+// int rows = ReadInt("Введите количество строк: ");
+// int cols = ReadInt("Введите количество столбцов: ");
+
+// var myMatrix = GenerateMatrix(rows, cols);
+// PrintMatrix(myMatrix);
+// System.Console.WriteLine();
+// Count(myMatrix);
+
+
+//  ИЛИ ********************************
+
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int[,] GenerateMatrix(int rows, int cols)
+// {
+//     Random rand = new Random();
+//     int[,] matrix = new int[rows, cols];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rand.Next(0, 11);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i, j] + "\t"); // \t - это табуляция (tab)
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void Count(int[,] matrix)
+// {
+//     var count = new int[10]; // частотный массив
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             count[matrix[i, j]]++;   // увеличение значения в индексе для подсчета
+//         }
+//     }
+//     for (int i = 0; i < count.Length; i++) // этот цикл должен быть в отдельной функции
+//     {
+//         if (count[i] != 0)
+//         {
+//             System.Console.WriteLine($"{i} встречается {count[i]} раз");
+//         }
+//     }
+
+// }
+
+
+// int rows = ReadInt("Введите количество строк: ");
+// int cols = ReadInt("Введите количество столбцов: ");
+
+// var myMatrix = GenerateMatrix(rows, cols);
+// PrintMatrix(myMatrix);
+
+// Count(myMatrix);
+
+
+
+// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, ----------------------------------------------------
+//            которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.-------------------
+
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int[,] GenerateMatrix(int rows, int cols)
+// {
+//     Random rand = new Random();
+//     int[,] matrix = new int[rows, cols];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rand.Next(0, 11);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i, j] + "\t"); // \t - это табуляция (tab)
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void Min(int[,] matrix, out int minI, out int minJ)
+// {
+//     minI = 0;
+//     minJ = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (matrix[minI, minJ] > matrix[i, j])
+//             {
+//                 minI = i;
+//                 minJ = j;
+//             }
+//         }
+//     }
+// }
+
+// int[,] MinMatrix(int[,] matrix, int minI, int minj)
+// {
+//     int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+//     int row = 0; // это новый индексы для нового массива
+//     int col = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         if (i != minI)
+//         {
+//             col = 0;
+//             for (int j = 0; j < matrix.GetLength(1); j++)
+//             {
+//                 if (j != minj)
+//                 {
+//                     newMatrix[row, col] = matrix[i, j];
+//                     col++;
+//                 }
+
+//             }
+//             row++;
+//         }
+
+//     }
+//     return newMatrix;
+
+// }
+
+// int rows = ReadInt("Введите количество строк: ");
+// int cols = ReadInt("Введите количество столбцов: ");
+
+// var myMatrix = GenerateMatrix(rows, cols);
+// PrintMatrix(myMatrix);
+
+// Min(myMatrix, out int minI, out int minJ);
+// System.Console.WriteLine();
+// PrintMatrix(MinMatrix(myMatrix, minI, minJ));
