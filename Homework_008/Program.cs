a91f424f159dba40d1cb7905fdc4202125f3b73d
1Mozart1 <andreivm2015@yandex.ru>
2023-02-23 13:09:06 +0500

    homework_008

--- Homework_008/Program.cs
+++ Homework_008/Program.cs
@@ -67,6 +67,76 @@
 //Задача 56: Задайте прямоугольный двумерный массив. 
 //           Напишите программу, которая будет находить строку с наименьшей суммой элементов.------------------------------------
 
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int[,] GenerateMatrix(int rows, int cols)
+// {
+//     Random rand = new Random();
+//     int[,] matrix = new int[rows, cols];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rand.Next(0, 11);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i, j] + "\t"); // \t - это табуляция (tab)
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+
+// void MinSumRow(int[,] matrix)
+// {
+//     int sumRow = 0;
+//     int minSum = 0;
+//     int row = 0;
+//     for (int i = 0; i < matrix.GetLength(1); i++)
+//     {
+//         minSum += matrix[0, i];
+//     }
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         sumRow = 0;
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             sumRow += matrix[i, j];
+//         }
+//         if (sumRow < minSum)
+//         {
+//             minSum = sumRow;
+//             row = i;
+//         }
+//     }
+//     System.Console.WriteLine($"Строка с минимальной суммой чисел № {row + 1} = {minSum}");
+// }
+
+// int rows = ReadInt("Введите количество строк: ");
+// int cols = ReadInt("Введите количество столбцов: ");
+
+// var myMatrix = GenerateMatrix(rows, cols);
+// PrintMatrix(myMatrix);
+
+// MinSumRow(myMatrix);
+
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.-----------------------
+
+
 int ReadInt(string text)
 {
     System.Console.Write(text);
@@ -99,36 +169,44 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-
-void MinSumRow(int[,] matrix)
+void Check(int[,] matrix1, int[,] matrix2)
 {
-    int sumRow = 0;
-    int minSum = 0;
-    int row = 0;
-    for (int i = 0; i < matrix.GetLength(1); i++)
+    if(matrix1.GetLength(1) != matrix2.GetLength(0))
     {
-        minSum += matrix[0, i];
+        System.Console.WriteLine($"Такие массивы перемножить нельзя");
     }
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        sumRow = 0;
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            sumRow += matrix[i, j];
-        }
-        if (sumRow < minSum)
-        {
-            minSum = sumRow;
-            row = i;
-        }
-    }
-    System.Console.WriteLine($"Строка с минимальной суммой чисел № {row + 1} = {minSum}");
+    
 }
 
-int rows = ReadInt("Введите количество строк: ");
-int cols = ReadInt("Введите количество столбцов: ");
+int[,] ResultMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] result = new int[matrix1.GetLength(0), matrix2.GetLongLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLongLength(1); j++)
+        {
+            result[i, j] = 0;
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                result[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+        }
+    }
+    return result;
+}
 
-var myMatrix = GenerateMatrix(rows, cols);
-PrintMatrix(myMatrix);
 
-MinSumRow(myMatrix);
+
+int rowsA = ReadInt("Введите количество строк матрицы А: ");
+int colsA = ReadInt("Введите количество столбцов матрицы А: ");
+var matrix1 = GenerateMatrix(rowsA, colsA);
+PrintMatrix(matrix1);
+System.Console.WriteLine();
+int rowsB = ReadInt("Введите количество строк матрицы Б: ");
+int colsB = ReadInt("Введите количество столбцов матрицы Б: ");
+var matrix2 = GenerateMatrix(rowsB, colsB);
+PrintMatrix(matrix2);
+System.Console.WriteLine();
+Check(matrix1, matrix2);
+int[,] ResMatrix = ResultMatrix(matrix1, matrix2);
+PrintMatrix(ResMatrix);
